753551c103d0f625d85e2f92bdb8447756c8cf85
anggun26 <anggunanisa1@gmail.com>
2021-11-15 13:59:04 +0700

    Mengubah Message

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require("express");
-
 const app = express();
 const port = process.env.PORT || 3000;
-
 
 app.get('/', (req, res) => {
     res.status(200).json({message : "Yeay Berhasil Hore"})
